2cdcdfc57768ebcbee4c6cabd23acdbb9f2895a0
Geoff <gardgeoff95@email.arizona.edu>
2020-11-26 15:04:49 -0700

    bug cleanup added text file functionality

--- logic.js
+++ logic.js
@@ -2,25 +2,65 @@ $(document).ready(() => {
   let stateMachine = {
     currentWindow: "none",
     currentCommand: "",
+    firstLine: true
   };
-  let textCounter = 0;
+  let windows = ["console", "text-file"]
+  let charIndex = 0
+  let zIndexCounter = 1;
+  function typeWriter(string, i, speed) {
+    console.log(string.length)
+    if (charIndex < string.length) {
+      $(".text-output").append(string.charAt(charIndex));
+      charIndex++;
+      setTimeout(function() {
+        typeWriter(string, charIndex, speed)
+      })
+    } else {
+      charIndex = 0;
+    }
+
+  }
+  
   $(".draggable").draggable({
     cancel: "div.non-draggable",
     containment: "body",
   });
-  $(".console").on("click", () => {
-    if (stateMachine.currentWindow != "console") {
+
+  $("div").on("click", function () {
+    if ($(this).hasClass("console")) {
+      
       stateMachine.currentWindow = "console";
-      console.log(stateMachine.currentWindow);
+      $(".console").css("z-index", zIndexCounter)
+      zIndexCounter++
+      
+    } else if ($(this).hasClass("text-file")) {
+    
+      stateMachine.currentWindow = "text-file"
+      $(".text-file").css("z-index", zIndexCounter)
+      zIndexCounter++
+
     }
   });
-  $(".command-prompt").dblclick(() => {
+  $(".icon").dblclick( function() {
+    if ($(this).hasClass("console-icon")){
     $(".console").fadeIn("fast");
     stateMachine.currentWindow = "console";
+    }
+    if($(this).hasClass("help-icon")) {
+      
+      $(".text-file").fadeIn("fast");
+      stateMachine.currentWindow = "text-file"
+
+    }
   });
-  $(".close-button").on("click", () => {
-    $(".console").fadeOut("fast");
-    $(".text-input").empty();
+  $(".close-button").on("click", function (e) {
+    let appType = $(this).attr("app-type");
+    if (appType == "console") {
+      $(".console").fadeOut("fast");
+      $(".text-input").empty();
+    } else if (appType == "text-file") {
+      $(".text-file").fadeOut("fast");
+    }
     stateMachine.currentWindow = "none";
   });
   $(".console").resizable();
@@ -32,22 +72,35 @@ $(document).ready(() => {
       if (e.keyCode == 8) {
         $(".text-input").text((_, txt) => {
           return txt.slice(0, -1);
-
-        })
+        });
       }
       if (e.keyCode == 13) {
         if (currentCommand == "help") {
-          $(".text-output").append(`${currentCommand} <br />List of Commands: <br /> Hello <br/>`);
-          $(".text-input").empty()
-          $('.io').stop().animate({
-            scrollTop: $('.io')[0].scrollHeight
-          }, 800);
+          // $(".text-output").empty();
+          if (!stateMachine.firstLine) {
+            $(".text-output").append("<br /><br />")
+
+          } 
+          stateMachine.firstLine =false; 
+          let helpString = "this is the response for the help input"
+          typeWriter(helpString, 0, 100);
+
+          charIndex = 0;
+          
+     
+          $(".text-input").empty();
+          $(".io")
+            .stop()
+            .animate(
+              {
+                scrollTop: $(".io")[0].scrollHeight,
+              },
+              800
+            );
         }
       }
       if ((e.keyCode >= 48 && e.keyCode <= 90) || e.keyCode == 32) {
- 
-          $(".text-input").append(e.key);
-        
+        $(".text-input").append(e.key);
       }
     }
   });
